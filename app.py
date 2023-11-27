1c09bda8dda642328009b942fed2f97527d82bba
Jony <jony.ghosh.jgh@gmail.com>
2023-11-27 15:21:11 +0600

    vehicle counting by user given line macanism working fine

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import numpy as np
 import torch
 
 model = torch.hub.load('/home/hb/Desktop/dynamic/yolov5', 'custom', path='/home/hb/Desktop/dynamic/yolov5/yolov5s.pt', source='local')  # PyTorch
-# model = torch.hub.load('ultralytics/yolov5', 'yolov5s', trust_repo=True)
 
 # Global variables to store the line coordinates
 line_start = (-1, -1)
@@ -11,8 +10,8 @@ line_end = (-1, -1)
 draw_line = False
 
 # Scale factor for resizing bounding boxes
-scale_factor_h = 0.3  # You can adjust this value as needed
-scale_factor_w = 0.15  # You can adjust this value as needed
+scale_factor_h = 0.35  # You can adjust this value as needed
+scale_factor_w = 0.25  # You can adjust this value as needed
 
 # Counting variables
 vehicle_count = 0
@@ -21,11 +20,55 @@ prev_vehicle_count = 0
 # Variable to keep track of vehicles that have crossed the line
 vehicles_crossed = set()
 
-# Function to check if a bounding box intersects with a line
-def is_intersecting_line(x1, y1, x2, y2, line_start, line_end):
-    # Use a simple check for intersection between bounding box and line
-    return cv2.pointPolygonTest(np.array([line_start, line_end], np.int32), (x1, y1), False) >= 0 or \
-           cv2.pointPolygonTest(np.array([line_start, line_end], np.int32), (x2, y2), False) >= 0
+
+# Function to check if two line segments intersect
+def do_line_segments_intersect(p1, p2, p3, p4):
+    def orientation(p, q, r):
+        val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+        if val == 0:
+            return 0
+        return 1 if val > 0 else 2
+
+    def on_segment(p, q, r):
+        return (q[0] <= max(p[0], r[0])) and (q[0] >= min(p[0], r[0])) and (q[1] <= max(p[1], r[1])) and (q[1] >= min(p[1], r[1]))
+
+    o1 = orientation(p1, p2, p3)
+    o2 = orientation(p1, p2, p4)
+    o3 = orientation(p3, p4, p1)
+    o4 = orientation(p3, p4, p2)
+
+    if o1 != o2 and o3 != o4:
+        return True
+
+    if o1 == 0 and on_segment(p1, p3, p2):
+        return True
+    if o2 == 0 and on_segment(p1, p4, p2):
+        return True
+    if o3 == 0 and on_segment(p3, p1, p4):
+        return True
+    if o4 == 0 and on_segment(p3, p2, p4):
+        return True
+
+    return False
+
+# Function to check if a bounding box intersects with a line segment
+def is_bbox_intersecting_line(x, y, w, h, line_start, line_end):
+    # Line segment represented by two points
+    p1 = np.array(line_start, dtype=np.float32)
+    p2 = np.array(line_end, dtype=np.float32)
+
+    # Bounding box represented by four corners
+    bbox_corners = np.array([(x, y), (x + w, y), (x, y + h), (x + w, y + h)], dtype=np.float32)
+
+    # Check for intersection
+    for i in range(4):
+        j = (i + 1) % 4
+        p3, p4 = bbox_corners[i], bbox_corners[j]
+        # Check if the line segments intersect
+        if do_line_segments_intersect(p1, p2, p3, p4):
+            return True
+
+    return False
 
 # Callback function for mouse events
 def mouse_callback(event, x, y, flags, param):
@@ -48,7 +91,7 @@ cv2.setMouseCallback('VideoFrame', mouse_callback)
 cap = cv2.VideoCapture('Data/video.mp4')
 
 while True:
-    print(f"vehicle no: {vehicle_count}")
+    # print(f"Vehicle count: {vehicle_count}")
     # Capture a frame from the video
     ret, frame = cap.read()
 
@@ -61,38 +104,39 @@ while True:
     res = model(frame)
 
     boxes = res.xyxy[0].tolist()
+    
     boxno = 1
     for box in boxes:
-        (x, y, w, h, _, _) = box
-        x, y, w, h = int(x), int(y), int(w), int(h)
+        (x, y, w, h, _, clss) = box
+        x, y, w, h, clss= int(x), int(y), int(w), int(h), int(clss)
+        
+        if clss == 1 or clss == 2 or clss == 3 or clss == 5 or clss == 7:
+            # Ensure coordinates are non-negative
+            x, y = max(0, x), max(0, y)
 
-        # Ensure coordinates are non-negative
-        x, y = max(0, x), max(0, y)
+            # Scale down the width and height of the bounding box
+            w, h = int(w * scale_factor_w), int(h * scale_factor_h)
 
-        # Scale down the width and height of the bounding box
-        w, h = int(w * scale_factor_w), int(h * scale_factor_h)
+            cx = int((x + x + w) / 2)
+            cy = int((y + y + h) / 2)
+            # print("Box No:", boxno, " ", x, y, w, h)
 
-        cx = int((x + x + w) / 2)
-        cy = int((y + y + h) / 2)
-        # print("Box No:", boxno, " ", x, y, w, h)
+            cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        # Check if the bounding box intersects with the line
-        if draw_line and is_intersecting_line(x, y, x + w, y + h, line_start, line_end):
+            # Check if the bounding box intersects with the line
+            
+            if draw_line and is_bbox_intersecting_line(x, y, w, h, line_start, line_end):
             # Check if the vehicle is not already marked as crossed
-            if boxno not in vehicles_crossed:
-                vehicles_crossed.add(boxno)
-                vehicle_count += 1
+                if boxno not in vehicles_crossed:
+                    vehicles_crossed.add(boxno)
+                    vehicle_count += 1
 
         boxno += 1
 
     # Draw the dynamic line if both start and end points are set
     if draw_line:
         cv2.line(frame, line_start, line_end, (0, 255, 0), 2)
-        
-    
 
     # Display the vehicle count
     cv2.putText(frame, f"Vehicle Count: {vehicle_count}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
--- object_detection.py
+++ object_detection.py
@@ -1,7 +1,7 @@
 # YOLOv5 PyTorch HUB Inference (DetectionModels only)
 import torch
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', force_reload=True, trust_repo=True)  # or yolov5n - yolov5x6 or custom
+model = torch.hub.load('/home/hb/Desktop/dynamic/yolov5', 'custom', path='/home/hb/Desktop/dynamic/yolov5/yolov5s.pt', source='local')  # PyTorch
 im = 'Data/image.jpeg'  # file, Path, PIL.Image, OpenCV, nparray, list
 results = model(im,augment=False)  # inference
 boxes = results.xyxy[0].tolist()
@@ -9,7 +9,7 @@ boxes = results.xyxy[0].tolist()
 boxno = 1
 print(boxes)
 for box in boxes:
-    x,y,w,h,_,_ = box
+    x,y,w,h,_,clss= box
     cx = int((x + x + w)/2)
     cy = int((y+y+h)/2)
     print("Box No:", boxno, " ", x, y, w, h)
